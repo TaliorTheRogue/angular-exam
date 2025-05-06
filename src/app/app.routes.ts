60bc6ef0a01fc9eba952880f75cf3248508f3228
TaliorTheRogue <donribeiro45@gmail.com>
2025-05-06 10:32:29 +0200

    Main Architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { authRoutes } from './routes/auth.routes';
 
-export const routes: Routes = [];
+const appTitle = "Angular-Exam";
+
+export const routes: Routes = [
+  {
+    path: "",
+    title: `Accueil - ${appTitle}`,
+    pathMatch: "full",
+    loadComponent: () => import("./components/landing-page/landing-page.component").then(m => m.LandingPageComponent)
+  },
+  {
+    path: "auth",
+    loadChildren: () => import("./routes/auth.routes").then(m => m.authRoutes)
+  },
+  {
+    path: "error/404",
+    title: `Erreur 404 - ${appTitle}`,
+    loadComponent: () => import("./components/error/error.component").then(m => ErrorComponent)
+  },
+  {
+    path: "**",
+    redirectTo: "erreur/404"
+  }
+];
